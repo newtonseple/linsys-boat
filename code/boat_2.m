c7a44dc633779dea67a983f864e7ba323b769591
Unknown <newtonseple@gmail.com>
2016-11-03 15:44:28 +0100

    Small fix

--- code/boat_2.m
+++ code/boat_2.m
@@ -19,7 +19,7 @@ omega_0 = f(omega_0_ind)
 %% 5.2d - find lambda
 sigma = sqrt(pxx_max);
 lambda_0 = 1; % past: 0.27 Initial guess lambda
-% x is lambda, o is omega
+% l is lambda, o is omega
 K_w = @(l) 2*l*omega_0*sigma
 %PSD = @(l, xdata) arrayfun(@(o) ((2*l*omega_0*sigma*o)^2/((omega_0^2 - o^2)^2+(2*l*omega_0*o)^2))^2 ,xdata)
 PSD = @(l, xdata) arrayfun(@(o) ((K_w(l))^2 * o^2 / (o^4 + 2*omega_0^2 * (2*l^2 - 1) * o^2 + omega_0^4)) ,xdata)
