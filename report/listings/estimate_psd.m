a729a6d8120826ebee5a8000c1177735cd1e07b6
Unknown <newtonseple@gmail.com>
2016-11-19 16:53:45 +0100

    update

--- report/listings/estimate_psd.m
+++ report/listings/estimate_psd.m
@@ -12,8 +12,5 @@ omega_0 = f(omega_0_ind)  % Extract said frequency
 %% 2d - find lambda
 sigma = sqrt(pxx_max);  % Sigma is needed in PSD
 lambda_0 = 1;  % Initial guess for lambda
-
-% PSD function (o is omega, l is lambda)
-PSD = @(l, xdata) arrayfun(@(o) (2*l*omega_0*sigma)^2 * o^2 / (o^4 + 2*omega_0^2 * (2*l^2 - 1) * o^2 + omega_0^4)) ,xdata)
-
+PSD = @(l, xdata) arrayfun(@(o) (2*l*omega_0*sigma)^2 * o^2 / (o^4 + 2*omega_0^2 * (2*l^2 - 1) * o^2 + omega_0^4)) ,xdata)  % PSD function (o is omega, l is lambda)
 lambda = lsqcurvefit(PSD,lambda_0,f,pxx)  % Estimate lambda by fitting PSD to the Welch estimate from 2a
